0286085da60dcfd8c3a8908c440d6edfd0e6f221
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2025-03-02 19:49:55 +0100

    like this .... maybe?

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="useful_utility",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=["colorama", "iniconfig", "numpy", "packaging", "pluggy", "pytest", "setuptools"],
     long_description=open('README.md').read(),
